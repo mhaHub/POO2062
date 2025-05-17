678e8fa33b814a26f82d820a31bfb44812097b9c
mhaHub <123046406@upq.edu.mx>
2025-05-16 18:36:27 -0600

    Solicitud de datos, boolean, comparaciones y logicos

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -24,10 +24,32 @@ x= 4
 x= 5.76
 print(x)
 
-x= int(3)
-y= float(3)
-z= str(3)
-print(x,y,z)
-print(type(x))
-print(type(y))
-print(type(z))
+#x= int(3)
+#y= float(3)
+#z= str(3)
+#print(x,y,z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+#3. solicitud de datos
+
+#a= input("introduce cualquier dato: ")
+#b= int(input("introduce un numero entero: "))
+#c= float(input("introduce un numero decimal: "))
+
+#print(a,b,c)
+
+#4. boolean, comparacion y logicos
+
+print(10 > 9)
+print(10 < 9)
+print(10 == 9)
+print(10 >= 9)
+print(10 <= 9)
+print(10 != 9)
+
+x=1
+print(x<5 and x<10)
+print(x<5 or x<10)
+print(not(x<5 and x<10))
