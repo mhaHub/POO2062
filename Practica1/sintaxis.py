9f4086b9d6dcfe59640d90f24b46a184e6163816
mhaHub <123046406@upq.edu.mx>
2025-05-16 18:19:03 -0600

    Variables

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -9,5 +9,25 @@ varia lineas en python """
 
 
 #2. Strings
-print("hola soy una cadena")
 print('yo soy otra')
+
+variable1 = "hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. Variables
+
+x= "Diego Rivera"
+x= 4
+x= 5.76
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
