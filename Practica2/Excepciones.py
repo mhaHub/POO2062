0edf38436d8fcebd14b2acc23d1675de5264a4a1
mhaHub <123046406@upq.edu.mx>
2025-05-21 15:20:41 -0600

    Multiples Excepciones

--- Practica2/Excepciones.py
+++ Practica2/Excepciones.py
@@ -1,11 +1,17 @@
 #Manejo de Excepciones
+#try:
+    #numero = int(input("Introduce un numero: "))
+    #resultado = 10 / numero
+
+    #print("Resultado:", resultado)
+
+#except ValueError:
+    #print("Error: Se ingreso algo que no es un numero entero.")
+#except ZeroDivisionError:
+    #print("Error: Estas intentando dividir entre 0")
+    
+#Multiples Excepciones
 try:
-    numero = int(input("Introduce un numero: "))
-    resultado = 10 / numero
-
-    print("Resultado:", resultado)
-
-except ValueError:
-    print("Error: Se ingreso algo que no es un numero entero.")
-except ZeroDivisionError:
-    print("Error: Estas intentando dividir entre 0")
+    resultado = 10 / 0
+except(ZeroDivisionError, TypeError) as error:
+    print(f"!Error! {error}")
