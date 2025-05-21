66a81d6b4cf6342cdcca9c7862fd899e4e927ccf
mhaHub <123046406@upq.edu.mx>
2025-05-21 15:58:21 -0600

    Uso del Raise

--- Practica2/Excepciones.py
+++ Practica2/Excepciones.py
@@ -1,6 +1,6 @@
-#Manejo de Excepciones
+#1. Manejo de Excepciones
 #try:
-    #numero = int(input("Introduce un numero: "))
+   # numero = int(input("Introduce un numero: "))
     #resultado = 10 / numero
 
     #print("Resultado:", resultado)
@@ -10,7 +10,12 @@
 #except ZeroDivisionError:
     #print("Error: Estas intentando dividir entre 0")
     
-#Multiples Excepciones
+#2. Multiples Excepciones
+#try:
+    #resultado = 10 / 0
+#except(ZeroDivisionError, TypeError) as error:
+    #print(f"!Error! {error}")
+
 #3. Finally
 try:
     numero = int(input("Introduce un numero: "))
@@ -24,3 +29,20 @@ except ZeroDivisionError:
     print("Error: Estas intentando dividir entre 0")
 finally:
     print("Cerrando Programa...")
+    
+#Usar Raise
+
+def validar_edad(edad):
+    if edad < 0:
+        raise ValueError("La edad no puede ser negativa")
+    if edad < 18:
+        raise ValueError("Debes ser mayor de edad")
+    print("Edad válida")
+
+try:
+    edad = int(input("Ingrese su edad: "))
+    validar_edad(edad)
+except ValueError as e:
+    print(f"Error: {e}")
+else:
+    print("Puedes continuar")
