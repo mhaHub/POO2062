a9044ba4509934a267e822f6a1c5fe4b329f0886
mhaHub <123046406@upq.edu.mx>
2025-06-12 20:05:48 -0600

    Tipos de Rutas en Flask
    Aqui se pusieron a prueba los diferentres tipos de rutas que existen en Flask

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -3,9 +3,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Ruta simple
 @app.route('/')
 def home():
     return 'Hola Mundo FLASK'
+
+#Ruta con parametros
+@app.route('/saludo/<nombre>')
+def saludar(nombre):
+    return 'Hola, '+nombre+'!!!'
+
+#Ruta try-catch
+@app.errorhandler(404)
+def paginaNoE(e):
+    return 'Cuidado: Error de capa 8 !!!', 404
+
+#Ruta doble
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleroute():
+    return 'Soy el mismo recurso del servidor'
+
+#Ruta POST
+@app.route('/formulario', methods = ['POST'])
+def formulario():
+    return 'Soy un formulario'
+
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
