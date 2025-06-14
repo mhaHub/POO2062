055ea2ee60d885234b41b576211fe05e89ed2ad7
mhaHub <123046406@upq.edu.mx>
2025-06-13 18:37:00 -0600

    Conexion a la base de datos con Flask
    Se importaron algunas librerias MySQL y se creo una ruta en la cual este se puede concetar a una base de  datos

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -1,7 +1,26 @@
 
-from flask import Flask
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] ="localhost"
+app.config['MYSQL_USER'] ="root"
+app.config['MYSQL_PASSWORD'] ="12345678"
+app.config['MYSQL_DB'] ="dbflask"
+#app.config['MYSQL_PORT'] =3306 // usar solo en cambio de puerto
+
+mysql = MySQL(app)
+#Ruta para checar la conexion a mysql
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify( {'status':'ok','message':'Conectado con exito.'} ),200
+    except MySQLdb.MySQLError as e:
+        return jsonify( {'status':'error','message':str(e)} ),500
 
 #Ruta simple
 @app.route('/')
